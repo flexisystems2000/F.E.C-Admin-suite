f8d9ecd3e02cc0e7c24bb745241cdf03e6ffdf6e
flexisystems2000 <flexisystems2000@gmail.com>
2026-08-14 23:18:19 +0100

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flexisystems.fecadmin',
+  appName: 'F.E.C Admin Suite',
+  webDir: 'www',
+
+  server: {
+    url: 'https://flexisystems2000.github.io/F.E.CAdminpanel/',
+    cleartext: false
+  },
+
+  android: {
+    backgroundColor: '#ffffff'
+  }
+};
+
+export default config;
